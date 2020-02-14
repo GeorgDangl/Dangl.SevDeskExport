b89e3ccf13dcfdd5013be4f256192a79a4e7f551
Georg Dangl <georg@dangl.me>
2020-02-14 22:22:33 +0100

    Add CLI tool

---
+++ Dangl.SevDeskExport/Program.cs
@@ -0,0 +1,34 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Threading.Tasks;
+
+namespace Dangl.SevDeskExport
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            HeadingInfo.Default.WriteMessage("Visit https://www.dangl-it.com to find out more about this exporter");
+            HeadingInfo.Default.WriteMessage("This generator is available on GitHub: https://github.com/GeorgDangl/Dangl.SevDeskExport");
+            await Parser.Default.ParseArguments<ApiExportOptions>(args)
+                .MapResult(async options =>
+                {
+                    try
+                    {
+                        var exporter = new Exporter(options);
+                        await exporter.ExportSevDeskDataAndWriteToDisk().ConfigureAwait(false);
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine(e.ToString());
+                    }
+                },
+                errors =>
+                {
+                    Console.WriteLine("Could not parse CLI arguments");
+                    return Task.CompletedTask;
+                }).ConfigureAwait(false);
+        }
+    }
+}
